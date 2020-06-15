aff41ffba0963d83ec580857fb00dd426b34cbc3
reckoner1429 <abhishekbisht1429@gmail.com>
2020-06-15 11:59:34 +0530

    node callbacks and error handling

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,12 +1,16 @@
 var rect = require('./rectangle')
 
 function solveRect(l,b) {
-    if(l<=0 || b<=0) {
-        console.log("lenght and bredth must be greater than zero")
-    } else {
-        console.log("Area : " + rect.area(l,b));
-        console.log("perimeter : " + rect.perimeter(l,b))
-    }
+    rect(l,b,(err, rectangle) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            console.log("Area : " + rectangle.area());
+            console.log("Perimeter : " + rectangle.perimeter());
+        }
+    });
+
+    console.log("This statement is after call to rect")
 }
 
 solveRect(2,4);
