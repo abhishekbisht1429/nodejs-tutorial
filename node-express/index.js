63e8d6fb054db7e1e0756e8c5bb7270cabb4f302
reckoner1429 <abhishekbisht1429@gmail.com>
2020-06-19 01:44:41 +0530

    added promoRouter and leaderRouter

--- node-express/index.js
+++ node-express/index.js
@@ -6,13 +6,17 @@ const bodyParser = require('body-parser');
 const hostname = 'localhost';
 const port = 3000;
 
-const dishRouter = require('./routes/dishRouter')
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
