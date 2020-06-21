91439378a2ae7c584c49cf1c5cfa4080ce16dd19
reckoner1429 <abhishekbisht1429@gmail.com>
2020-06-21 23:47:46 +0530

    authentication using passport

--- conFusionServer/models/users.js
+++ conFusionServer/models/users.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
